04fa0bcdd5a12735b7c294b80c6e5d2bc07d309b
devekar <vaibhav.devekar@gmail.com>
2013-09-07 23:30:00 -0400

    IO3

--- IO.py
+++ IO.py
@@ -25,25 +25,26 @@ class TitleParser(HTMLParser):
 path = r'reuters'
 data = {}
 parser = TitleParser()
-
-file = open("reuters/reut2-000.sgm")
-content = re.sub('&(.+?);', '',file.read())  #remove &xxx;
-parser.feed(content)
-file.close()
-
 d = defaultdict(int)
-for item in parser.data_list:
-   item = re.sub( '[<>]', '', item) #remove <, >
-   item = item.replace('\n','') #remove newlines
-   list = item.split()   #tokenize
-   for i in list:
-      d[i] += 1
+writer = csv.writer(open("out.txt","w"),delimiter = ':')
 
-file = open("out.txt","w")
-writer = csv.writer(file,delimiter = ':')
-for key, value in d.items():
-   writer.writerow([key, value])
+for dir_entry in os.listdir(path):
+    dir_entry_path = os.path.join(path, dir_entry)
+    file = open(dir_entry_path)
+    content = re.sub('&(.+?);', '',file.read())  #remove &xxx;
+    parser.feed(content)
+    file.close()
 
-file.close()
+    for item in parser.data_list:
+        item = re.sub( '[<>]', '', item) #remove <, >
+        item = item.replace('\n','') #remove newlines
+        list = item.split()   #tokenize
+        for i in list:
+            d[i] += 1
+
+    for key, value in d.items():
+        writer.writerow([key, value])
+
+
 
 
